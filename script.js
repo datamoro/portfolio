e02209efbfc918e350afa37bf6068cffa4f82269
Caio Moro <caio.moro@jadlog.com.br>
2026-01-10 11:45:19 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Basic interactions can go here
+console.log("Welcome to Caiom's Portfolio");
+
+// Optional: Add scroll reveal logic if we want more complex animations than CSS keyframes
+document.addEventListener('DOMContentLoaded', () => {
+    // Future enhancements
+});
